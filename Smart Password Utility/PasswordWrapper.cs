4ea632d196b531c844dc115fcd7f85946353b1dd
Ben Grewell <bgrewelldev@gmail.com>
2018-02-26 18:01:49 -0800

    initial commit

---
+++ Smart Password Utility/PasswordWrapper.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Password_Utility
+{
+
+    class PasswordWrapper
+    {
+        private const string API_URL = "https://api.pwnedpasswords.com/range/{0}";
+
+        /// #define NET_API_STATUS DWORD
+        /// </summary>
+        public enum NET_API_STATUS : uint
+        {
+            NERR_Success = 0,
+            NERR_InvalidComputer = 2351,
+            NERR_NotPrimary = 2226,
+            NERR_SpeGroupOp = 2234,
+            NERR_LastAdmin = 2452,
+            NERR_BadPassword = 2203,
+            NERR_PasswordTooShort = 2245,
+            NERR_UserNotFound = 2221,
+            ERROR_ACCESS_DENIED = 5,
+            ERROR_NOT_ENOUGH_MEMORY = 8,
+            ERROR_INVALID_PASSWORD = 86,
+            ERROR_INVALID_PARAMETER = 87,
+            ERROR_INVALID_NAME = 123,
+            ERROR_INVALID_LEVEL = 124,
+            ERROR_MORE_DATA = 234,
+            ERROR_SESSION_CREDENTIAL_CONFLICT = 1219,
+            RPC_S_SERVER_UNAVAILABLE = 2147944122,
+            RPC_E_REMOTE_DISABLED = 2147549468
+        }
+
+        [DllImport("netapi32.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.StdCall, SetLastError = true)]
+        public static extern NET_API_STATUS NetUserChangePassword([MarshalAs(UnmanagedType.LPWStr)] string domainname, [MarshalAs(UnmanagedType.LPWStr)] string username,
+                                                                  [MarshalAs(UnmanagedType.LPWStr)] string oldpassword,[MarshalAs(UnmanagedType.LPWStr)] string newpassword);
+
+        [DllImport("msvcrt.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern int memcmp(byte[] b1, byte[] b2, UIntPtr count);
+
+        static PasswordWrapper()
+        {
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+        }
+
+        public static int CheckIsPasswordPwned(string password)
+        {
+            int count = 0;
+            try
+            {
+                string passwordHash = GetSHA1Hash(password);
+                string prefix = passwordHash.Substring(0, 5);
+                string suffix = passwordHash.Substring(5);
+                Dictionary<string, int> results = QueryPwnedPasswordsAPI(prefix).GetAwaiter().GetResult();
+                if (results.ContainsKey(suffix))
+                {
+                    count = results[suffix];
+                }
+
+                return count;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                return -1;
+            }
+        }
+
+        private static string GetSHA1Hash(string password)
+        {
+            using (SHA1Managed hasher = new SHA1Managed())
+            {
+                var hash = hasher.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return string.Join("", hash.Select(x => x.ToString("X2")).ToArray());
+            }
+        }
+
+        private static async Task<Dictionary<string, int>> QueryPwnedPasswordsAPI(string prefix)
+        {
+            string url = API_URL.Replace("{0}", prefix);
+            WebClient client = new WebClient();
+            Stream responseStream = client.OpenRead(url);
+            StreamReader reader = new StreamReader(responseStream);
+            string response = reader.ReadToEnd();
+            if (string.IsNullOrEmpty(response)) 
+            {
+                return null;
+            }
+            string[] entries = response.Split(new char[] { '\r', '\n', '\t', ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> results = new Dictionary<string, int>();
+            foreach(string entry in entries)
+            {
+                string[] parts = entry.Split(':');
+                results.Add(parts[0], int.Parse(parts[1]));
+            }
+            return results;
+        }
+
+        public static bool SecureStringsMatch(SecureString s1, SecureString s2)
+        {
+            if (s1 == s2) return true; //reference equality check
+
+            if (s1 == null || s2 == null || s1.Length != s2.Length) return false;
+            IntPtr bstr1 = IntPtr.Zero;
+            IntPtr bstr2 = IntPtr.Zero;
+            try
+            {
+                bstr1 = Marshal.SecureStringToBSTR(s1);
+                bstr2 = Marshal.SecureStringToBSTR(s2);
+                byte b1 = 1;
+                byte b2 = 1;
+                int i = 0;
+                while (((char)b1) != '\0')
+                {
+                    b1 = Marshal.ReadByte(bstr1, i);
+                    b2 = Marshal.ReadByte(bstr2, i);
+                    if (b1 != b2)
+                    {
+                        return false;
+                    }
+                    i += 2;
+                }
+                return true;
+            }
+            finally
+            {
+                if (bstr1 != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(bstr1);
+                }
+                if (bstr2 != IntPtr.Zero)
+                {
+                    Marshal.ZeroFreeBSTR(bstr2);
+                }
+            }
+        }
+    }
+}
--- Smart Password Utility/Program.cs
+++ Smart Password Utility/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,84 @@ namespace Smart_Password_Utility
     {
         static void Main(string[] args)
         {
+            int maxTries = 3;
+            int tries = 0;
+            while (tries < maxTries)
+            {
+                // Prompt for old password
+                Console.Write("Old Password: ");
+                string oldPassword = ReadPasswordInput().ToString();
+
+                //prompt:
+                // Prompt for new password
+                Console.Write("New Password: ");
+                SecureString newPassword1 = ReadPasswordInput();
+
+                //// Check against the pwned password api
+                //var count = PasswordWrapper.CheckIsPasswordPwned(newPassword1);
+
+                //// If password was found warn and prompt to try a different password
+                //if (count > 0)
+                //{
+                //    Console.WriteLine("This password has been found {0:N0} times in data breaches. Please try another password.\r\n", count);
+                //    goto prompt;
+                //} 
+
+                // Once we are good confirm the password
+                Console.Write("Confirm Password: ");
+                SecureString newPassword2 = ReadPasswordInput();
+                if (!PasswordWrapper.SecureStringsMatch(newPassword1, newPassword2))
+                {
+                    Console.WriteLine("Passwords don't match. Try again!\r\n");
+                    continue;
+                } else
+                {
+                    Console.WriteLine("Passwords match!");
+                }
+            //    } else
+            //    {
+            //        var result = PasswordWrapper.NetUserChangePassword(Environment.UserDomainName, "bob", oldPassword, newPassword1);
+            //        if (result == 0)
+            //        {
+            //            Console.WriteLine("Password Changed!");
+            //            break;
+            //        }
+
+            //        Console.WriteLine("Error: {0} [{1} tries remaining]", result, maxTries-tries-1);
+            //        tries++;
+            //    }
+            }
+
+            Console.WriteLine("Press any key to exit ... remove me");
+            Console.ReadKey();
+        }
+
+        public static SecureString ReadPasswordInput()
+        {
+            SecureString pass = new SecureString();
+            while (true)
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Backspace)
+                {
+                    if (pass.Length > 0)
+                    {
+                        pass.RemoveAt(pass.Length - 1);
+                        Console.Write("\b \b");
+
+                    }
+                } else if (key.Key == ConsoleKey.Enter)
+                {
+                    break;
+                } else
+                {
+                    pass.AppendChar(key.KeyChar);
+                    Console.Write("*");
+                }
+            }
+
+            Console.WriteLine("");
+            return pass;
         }
     }
 }
